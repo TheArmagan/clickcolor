722fcd718eeabbd8afec22663e0f7341591bacef
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2020-09-11 08:11:48 +0300

    update create session button text

--- public/js/index.page.js
+++ public/js/index.page.js
@@ -1,11 +1,22 @@
 const sessionIdInput = document.querySelector("#sessionIdInput");
-const createSessionButton = document.querySelector("#createSessionButton");
+const createOrJoinSessionButton = document.querySelector("#createOrJoinSessionButton");
 const titleElement = document.querySelector("#title");
 
 sessionIdInput.value = randomText();
 
-createSessionButton.addEventListener("click",()=>{
+createOrJoinSessionButton.addEventListener("click",()=>{
     if (!sessionIdInput.value) sessionIdInput.value = randomText();
     localStorage.setItem("setSessionColorTo", RANDOM_HEX);
     window.location = `/session/${sessionIdInput.value}`;
 })
+
+function updateCreateOrJoinSessionButtonTextContent(i=1) {
+    if (createOrJoinSessionButton.textContent == "Create Session") {
+        createOrJoinSessionButton.textContent = "Join To Session";
+    } else {
+        createOrJoinSessionButton.textContent = "Create Session";
+    }
+    setTimeout(()=>{updateCreateOrJoinSessionButtonTextContent()},5000);
+}
+
+updateCreateOrJoinSessionButtonTextContent();
